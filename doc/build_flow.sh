2e9e3ddcec82a26b827863c8be1012f3c22b9633
Michelle Daya <michelle.daya@ucdenver.edu>
2016-02-09 14:33:23 -0700

    Comitting final outline of update flows

--- doc/build_flow.sh
+++ doc/build_flow.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bash ../../utility_scripts/build_flow.sh flow.dot flow.png ../data/output/flow_nrs.txt
+bash ../../utility_scripts/build_flow.sh 650_flow.dot 650_flow.png ../data/output/flow_nrs.txt
+bash ../../utility_scripts/build_flow.sh ped_flow.dot ped_flow.png ../data/output/flow_nrs.txt
