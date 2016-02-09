5f1043c6b5fb3e7f8415bc48a87f7f46792a3479
Michelle Daya <michelle.daya@ucdenver.edu>
2016-02-09 15:56:31 -0700

    Comitting changes so as to show how many singleton families are removed

--- doc/build_flow.sh
+++ doc/build_flow.sh
@@ -3,9 +3,9 @@
 #Set the numbers that are determined manually
 n_clean_omni=`wc -l ../data/input/omni_clean.fam | tr -s ' ' | cut -f2 -d' '`
 n_del_unresolved=1
-n_fixed_omni="$(($n_clean_omni-$n_del_unresolved))"
+n_parents_fixed_omni="$(($n_clean_omni-$n_del_unresolved))"
 n_ped_unresolved=2
-echo "n_fixed_omni $n_fixed_omni" >> ../data/output/flow_nrs.txt
+echo "n_parents_fixed_omni $n_parents_fixed_omni" >> ../data/output/flow_nrs.txt
 echo "n_del_unresolved $n_del_unresolved" >> ../data/output/flow_nrs.txt
 echo "n_ped_unresolve $n_ped_unresolve" >> ../data/output/flow_nrs.txt
 
