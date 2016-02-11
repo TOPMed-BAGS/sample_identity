c6b28fb5db7d6a19c4a5f55a34b44bd500eef98b
Michelle Daya <michelle.daya@ucdenver.edu>
2016-02-11 14:29:46 -0700

    Fixed order in which parameters are passed

--- scripts/7_run_ibd_after_fixes.sh
+++ scripts/7_run_ibd_after_fixes.sh
@@ -6,7 +6,7 @@ grep pedigree ../data/output/omni_delete.txt | cut -f1 >> tmp_c.txt
 paste tmp_c.txt tmp_c.txt > tmp_delete_ids.txt
 
 #Run IBD between clean 650 and Omni data
-bash run_ibd.sh ../data/output/fixed_sample_map.txt ../data/input/fixed_pedigrees.txt tmp_delete_ids.txt ../data/output/fixed
+bash run_ibd.sh ../data/output/fixed_sample_map.txt ../data/input/fixed_pedigrees.txt ../data/output/fixed tmp_delete_ids.txt
 
 #Cleanup tmp files
 rm tmp_*
