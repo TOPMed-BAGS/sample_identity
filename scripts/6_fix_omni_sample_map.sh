300e024c2ec298f79246775ee2d76a950b08b3fd
Michelle Daya <michelle.daya@gmail>
2016-02-10 20:02:34 -0700

    Reporting nr of swaps

--- scripts/6_fix_omni_sample_map.sh
+++ scripts/6_fix_omni_sample_map.sh
@@ -3,3 +3,13 @@
 bash manual_omni_fixes.sh
 
 cat fix_omni_sample_map.R | R --vanilla
+
+n_ped_del=`grep pedigree ../data/output/omni_delete.txt | wc -l | tr -s ' '`
+n_ped_swap=`grep pedigree ../data/output/omni_swaps.txt | wc -l | tr -s ' '`
+n_ped_errors="$(($n_ped_del+$n_ped_swap))"
+n_ped_fixed=`wc -l ../data/output/fixed_sample_map.txt | tr -s ' ' | cut -f2 -d' '`
+n_ped_fixed="$(($n_ped_fixed-1))"
+echo "n_ped_del $n_ped_del" >> ../data/output/flow_nrs.txt
+echo "n_ped_swap $n_ped_swap" >> ../data/output/flow_nrs.txt
+echo "n_ped_errors $n_ped_errors" >> ../data/output/flow_nrs.txt
+echo "n_ped_fixed $n_ped_fixed" >> ../data/output/flow_nrs.txt
