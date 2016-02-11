ac5e50d3cf6474590be3f67914342c0358bb8a85
Michelle Daya <michelle.daya@ucdenver.edu>
2016-02-11 15:49:41 -0700

    Minor fix

--- scripts/9_create_summary_figures.sh
+++ scripts/9_create_summary_figures.sh
@@ -13,17 +13,19 @@ bash extract_excl_genomes.sh ../data/output/initial.genome \
      ../data/working/initial_650.genome \
      650_ omni_
 
-bash scripts/extract_genomes.sh \
+bash extract_genomes.sh \
      ../data/output/fixed.genome  \
      ../data/working/fixed_650_omni_pairs.genome \
      650_ omni_
-bash scripts/extract_excl_genomes.sh ../data/output/fixed.genome \
+bash extract_excl_genomes.sh ../data/output/fixed.genome \
      ../data/working/fixed_omni.genome \
      omni_ 650_
-bash scripts/extract_excl_genomes.sh ../data/output/fixed.genome \
+bash extract_excl_genomes.sh ../data/output/fixed.genome \
      ../data/working/fixed_650.genome \
      650_ omni_
 
+rm -r ../data/output/figures
+mkdir ../data/output/figures
 cat create_summary_figures.R | R --vanilla
 
 rm -r ../data/working
