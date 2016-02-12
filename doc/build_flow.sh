9c52c01cca09e365f4593dfee5892a37685b23ac
Michelle Daya <michelle.daya@ucdenver.edu>
2016-02-12 10:51:51 -0700

    Added missing parameter

--- doc/build_flow.sh
+++ doc/build_flow.sh
@@ -3,10 +3,11 @@
 #Set the numbers that are determined manually
 n_clean_omni=`wc -l ../data/input/omni_clean.fam | tr -s ' ' | cut -f2 -d' '`
 n_del_unresolved=1
-n_parents_fixed_omni="$(($n_clean_omni-$n_del_unresolved))"
+n_fixed_omni="$(($n_clean_omni-$n_del_unresolved))"
 n_ped_unresolved=2
 n_sex_fix=1
-echo "n_parents_fixed_omni $n_parents_fixed_omni" >> ../data/output/flow_nrs.txt
+echo "n_fixed_omni $n_fixed_omni" >> ../data/output/flow_nrs.txt
+echo "n_parents_fixed_omni $n_fixed_omni" >> ../data/output/flow_nrs.txt
 echo "n_del_unresolved $n_del_unresolved" >> ../data/output/flow_nrs.txt
 echo "n_ped_unresolved $n_ped_unresolved" >> ../data/output/flow_nrs.txt
 
