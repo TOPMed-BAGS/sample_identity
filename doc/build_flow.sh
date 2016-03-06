6fd47c73046c83cbcafaa891824f2771a44217d8
Michelle Daya <michelle.daya@gmail>
2016-03-05 19:29:12 -0700

    Corrections to numbers recorded for flow diagram

--- doc/build_flow.sh
+++ doc/build_flow.sh
@@ -5,11 +5,14 @@ n_clean_omni=`wc -l ../data/input/omni_clean.fam | tr -s ' ' | cut -f2 -d' '`
 n_del_unresolved=1
 n_fixed_omni="$(($n_clean_omni-$n_del_unresolved))"
 n_ped_unresolved=2
-n_sex_fix=1
+n_sex_fixes=`wc -l ../data/output/pedigree_sex.txt | tr -s ' ' | cut -f2 -d' '`
+n_ped_del=`grep pedigree ../data/output/omni_delete.txt | wc -l | tr -s ' '`
+n_ped_fixed="$(($n_clean_omni-$n_ped_del))"
 echo "n_fixed_omni $n_fixed_omni" >> ../data/output/flow_nrs.txt
 echo "n_parents_fixed_omni $n_fixed_omni" >> ../data/output/flow_nrs.txt
 echo "n_del_unresolved $n_del_unresolved" >> ../data/output/flow_nrs.txt
 echo "n_ped_unresolved $n_ped_unresolved" >> ../data/output/flow_nrs.txt
+echo "n_ped_fixed $n_ped_fixed" >> ../data/output/flow_nrs.txt
 
 #Build the flow diagrams
 bash ../../utility_scripts/build_flow.sh flow.dot flow.png ../data/output/flow_nrs.txt
--- scripts/6_fix_omni_sample_map.sh
+++ scripts/6_fix_omni_sample_map.sh
@@ -7,10 +7,9 @@ cat fix_omni_sample_map.R | R --vanilla
 n_ped_del=`grep pedigree ../data/output/omni_delete.txt | wc -l | tr -s ' '`
 n_ped_swap=`grep pedigree ../data/output/omni_swaps.txt | wc -l | tr -s ' '`
 n_ped_errors="$(($n_ped_del+$n_ped_swap))"
-n_ped_fixed=`wc -l ../data/output/fixed_sample_map.txt | tr -s ' ' | cut -f2 -d' '`
-n_ped_fixed="$(($n_ped_fixed-1))"
 n_no_rhq=`grep no_rhq ../data/output/omni_delete.txt | wc -l | tr -s ' '`
-n_with_rhq="$(($n_ped_fixed-$n_no_rhq))"
+n_with_rhq=`wc -l ../data/output/fixed_sample_map.txt | tr -s ' ' | cut -f2 -d' '`
+n_with_rhq="$(($n_with_rhq-1))"
 echo "n_ped_del $n_ped_del" >> ../data/output/flow_nrs.txt
 echo "n_ped_swap $n_ped_swap" >> ../data/output/flow_nrs.txt
 echo "n_ped_errors $n_ped_errors" >> ../data/output/flow_nrs.txt
