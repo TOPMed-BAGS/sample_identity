d60383365c84673b7c769398e1b4430a61fc3a9c
Michelle Daya <michelle.daya@gmail>
2016-03-06 07:06:17 -0700

    Comitting flow diagram fixes

--- doc/build_flow.sh
+++ doc/build_flow.sh
@@ -7,12 +7,13 @@ n_fixed_omni="$(($n_clean_omni-$n_del_unresolved))"
 n_ped_unresolved=2
 n_sex_fixes=`wc -l ../data/output/pedigree_sex.txt | tr -s ' ' | cut -f2 -d' '`
 n_ped_del=`grep pedigree ../data/output/omni_delete.txt | wc -l | tr -s ' '`
-n_ped_fixed="$(($n_clean_omni-$n_ped_del))"
+n_ped_fixed="$(($n_fixed_omni-$n_ped_del))"
 echo "n_fixed_omni $n_fixed_omni" >> ../data/output/flow_nrs.txt
 echo "n_parents_fixed_omni $n_fixed_omni" >> ../data/output/flow_nrs.txt
 echo "n_del_unresolved $n_del_unresolved" >> ../data/output/flow_nrs.txt
 echo "n_ped_unresolved $n_ped_unresolved" >> ../data/output/flow_nrs.txt
 echo "n_ped_fixed $n_ped_fixed" >> ../data/output/flow_nrs.txt
+echo "n_sex_fixes $n_sex_fixes" >> ../data/output/flow_nrs.txt
 
 #Build the flow diagrams
 bash ../../utility_scripts/build_flow.sh flow.dot flow.png ../data/output/flow_nrs.txt
