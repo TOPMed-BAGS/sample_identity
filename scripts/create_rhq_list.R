626d9e41ab3650dcafb272b430e51a6ab3c8a942
Michelle Daya <michelle.daya@ucdenver.edu>
2016-03-07 13:38:11 -0700

    Corrected file to also include RHQ IDs for those samples that were given new linked IDs

--- scripts/create_rhq_list.R
+++ scripts/create_rhq_list.R
@@ -1,10 +1,10 @@
-new.rhq <- read.table("new_rhq.txt")  #A text file containing all the new RHQ IDs
+new.rhq <- read.table("new_rhqs.txt")  #A text file containing all the new RHQ IDs
 new.rhq$V2 <- "new_rhq"
 
 rhq.link.update <- read.table("new_id_link.txt", head=T, stringsAsFactors = F)
 rhq.link.update <- rhq.link.update[rhq.link.update$NEWID != rhq.link.update$PATIENT,]
 rhq.link.update <- rhq.link.update[-grep("NEW", rhq.link.update$PATIENT, ignore.case = T),]
-update.frame <- data.frame(V1=rhq.link.update$NEWID)
+update.frame <- data.frame(V1=rhq.link.update$NEWID[rhq.link.update$PATIENT %in% new.rhq$V1])
 update.frame$V2 <- "new_rhq"
 new.rhq <- rbind(new.rhq, update.frame)
 
@@ -19,7 +19,11 @@ child.rhqs <- child.rhqs[!duplicated(child.rhqs)]
 child.rhq <- data.frame(V1=child.rhqs)
 child.rhq$V2 <- "old_child_rhq"
 
-out.frame <- rbind(new.rhq, adult.rhq, child.rhq)
+fam.650 <- read.table("650.fam", stringsAsFactors = F)
+phenos.650 <- data.frame(V1=fam.650$V2)
+phenos.650$V2 <- "650"
+
+out.frame <- rbind(new.rhq, adult.rhq, child.rhq, phenos.650)
 out.frame$V1[out.frame$V1 == "155022001"] <- "15022001"
 out.frame$V1[out.frame$V1 == "5372002"] <- "15372002"
 write.table(out.frame, "rhqs.txt", sep="\t", quote=F, row.names=F, col.names=F)
