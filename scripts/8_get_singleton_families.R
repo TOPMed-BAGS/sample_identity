8b446c8d32b0c044b019d7abb26f69dc03caf736
Michelle Daya <michelle.daya@ucdenver.edu>
2016-04-14 09:25:28 -0600

    Removed old files

--- scripts/8_get_singleton_families.R
+++
@@ -1,4 +0,0 @@
-sample.map <- read.table("../data/output/fixed_sample_map.txt", head=T, stringsAsFactors = F)
-pedigrees <- read.csv("../data/input/fixed_pedigrees.txt", head=T, stringsAsFactors = F)
-
-merged <- merge(sample.map, pedigrees, by.x="barnes_id", by.y="PATIENT")
